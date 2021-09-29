a2572b44377a63b6ed7117ed4ae7788aa744628b
Hamza <hamza@zembratech.com>
2021-09-29 14:36:34 -0600

    assessment completed

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(document).ready(() => {
     
-    $('#close-explore-button').on('click', function() {
-        $(this).parent('div').addClass('hidden');
-        $('.header-box').removeClass('hidden');
+    $('#close-explore-button').on('click', () => {
+        $('.explore-box').toggleClass('hide-explore-box').removeClass('show-explore-box');
+        setTimeout(() => {
+            $('.explore-box').toggleClass('d-none')
+        }, 750)
+        $('.explore-box').css('clip-path', 'polygon(4% 0, 96% 0, 100% 43%, 100% 58%, 96% 100%, 5% 100%, 0 57%, 0 40%)')
+        $('.header-box').fadeIn(300);
     })
 
-    $('.header-details-box').on('click', () => {
-        $('.header-box').addClass('hidden');
-        $('.explore-box').removeClass('hidden');
+    $('.header-details-box svg').on('click', () => {
+        $('.header-box').fadeOut(300);
+        $('.explore-box').removeClass('d-none hide-explore-box').toggleClass('show-explore-box')
+          setTimeout(() => {
+            $('.explore-box').css('clip-path', 'inset(0 0 0 0)')
+        }, 350)
     })
 })
